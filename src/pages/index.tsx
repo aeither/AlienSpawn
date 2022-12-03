8304e0a4682b19a177a544fdcfb7a9fc825ed156
aeither <36173828+aeither@users.noreply.github.com>
2022-12-03 14:22:29 +0100

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,28 @@
 import { type NextPage } from "next";
-import InitiativeList from "../components/InitiativeList";
+import Link from "next/link";
 import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-[calc(100vh-64px)] flex-col bg-neutral-dark">
-        <div className="container flex flex-col items-start gap-8 px-4 py-16">
-          <div className="flex w-full items-center justify-between pb-12">
-            <div>
+      <main className="flex min-h-[calc(100vh-64px)] flex-col justify-center bg-neutral-dark bg-[url('/bghero.png')]">
+        <div className="container flex flex-col  gap-8 px-4 py-16">
+          <div className="flex w-full items-center justify-between py-12">
+            <div className="w-full items-center text-center">
               <h1 className="text-5xl font-extrabold italic tracking-tight text-white sm:text-[5rem]">
-                <span className="text-primary">Alien</span> Spawn
+                Play with your custom NFT
               </h1>
-              <p className="pt-4 text-lg text-neutral-light">
-                A dynamic NFT battle game on Evmos.
+              <p className="pt-4 text-lg text-white">
+                A no-code dynamic NFT generator battle game on Evmos.
               </p>
             </div>
-            <div>
-              <img src="svgs/isometric03.svg" alt="" className="w-48" />
-            </div>
           </div>
-          <InitiativeList />
+          <div className="w-full text-center">
+            <Link href={"/spawn"} className="btn bg-black text-white">
+              START
+            </Link>
+          </div>
         </div>
       </main>
     </>
